b1842bb1926f984ec33974d2b34b862de8e9d3d6
matrixpower1004 <matrixpower1004@gmail.com>
2023-12-22 02:24:43 +0900

    Feat: day 28 - 완전탐색 - 시뮬레이션
    - 백준 15552 빠른 A+B : https://www.acmicpc.net/problem/15552
    - 백준 2745 진법 변환 : https://www.acmicpc.net/problem/2745

---
+++ src/fastcampus/part1/chapter03_arrays/bj15552/Main.java
@@ -0,0 +1,29 @@
+package fastcampus.part1.chapter03_arrays.bj15552;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/**
+ * author         : Jisang Lee
+ * date           : 2023-12-21
+ * description    : 백준 15552 빠른 A+B
+ * https://www.acmicpc.net/problem/15552
+ */
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int T = Integer.valueOf(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < T; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int A = Integer.valueOf(st.nextToken());
+            int B = Integer.valueOf(st.nextToken());
+            sb.append(A + B).append("\n");
+        }
+        System.out.println(sb);
+    }
+}
