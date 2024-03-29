8131f9657d9f08e6e9a8faeafdc4998f79852b7c
Jisang Lee <matrixpower1004@gmail.com>
2024-03-29 15:25:54 +0900

    Feat: 백준 11725 트리의 부모 찾기
    - https://www.acmicpc.net/problem/11725
    - 그래프 이론, 그래프 탐색, 트리, 너비 우선 탐색, 깊이 우선 탐색

---
+++ src/fastcampus/part2/chapter3/tree/bj11725/Main.java
@@ -0,0 +1,54 @@
+package fastcampus.part2.chapter3.tree.bj11725;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+/**
+ * 백준 11725 트리의 부모 찾기: https://www.acmicpc.net/problem/11725
+ */
+public class Main {
+
+    static List<Integer>[] tree;
+    static int[] parents;
+    static boolean[] visited;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        tree = new ArrayList[N + 1];
+        parents = new int[N + 1];
+        visited = new boolean[N + 1];
+        for (int i = 0; i <= N; i++) {
+            tree[i] = new ArrayList<>();
+        }
+
+        for (int i = 0; i < N - 1; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int node1 = Integer.parseInt(st.nextToken());
+            int node2 = Integer.parseInt(st.nextToken());
+            tree[node1].add(node2);
+            tree[node2].add(node1);
+        }
+        find(1);
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 2; i <= N; i++) {
+            sb.append(parents[i]).append("\n");
+        }
+        System.out.println(sb);
+    }
+
+    private static void find(int nodeNum) {
+        visited[nodeNum] = true;
+        for (int node : tree[nodeNum]) {
+            if (!visited[node]) {
+                parents[node] = nodeNum;
+                find(node);
+            }
+        }
+    }
+}
